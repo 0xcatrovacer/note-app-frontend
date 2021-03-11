7f1f86a3a100c562a3432ee0ad5a5c1bb0f38373
Swarnab Garang <swarnabgarang@gmail.com>
2021-03-12 02:04:34 +0530

    added and styled Navbar component

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <div class="Navbar">
+            <div className="NavTitle">
+                <h1 className="Titletext">Noter</h1>
+            </div>
+            <div className="NavRouters">
+                <span className="routes">Dashboard</span>
+                <span className="routes">About</span>
+            </div>
+            <button className="CreateNote">Add Note</button>
+        </div>
+    )
+}
+
+export default Navbar
