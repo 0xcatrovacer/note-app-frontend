46911d95762269c400e91e57f20aff47214b986c
Swarnab Garang <swarnabgarang@gmail.com>
2021-03-12 16:50:26 +0530

    added notes array in Home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,34 @@
 import React from 'react'
 import './Home.css'
 
+import DeleteIcon from '@material-ui/icons/Delete';
+
 const Home = () => {
+
+    const notes = [{
+        content: 'Task 1',
+        completed: true,
+        createdAt: 'yesterday'
+    },
+    {
+        content: 'Task 2',
+        completed: true,
+        createdAt: 'daybefore'
+    }
+    ]
+
     return (
         <div className="Home">
             <h1 class='HomeNotes'>Notes</h1>
             <button className="AddBtn">+</button>
+            <div className="NoteList">
+                {notes.map((note) => (
+                    <div className="Note">
+                        <div className="NoteContent">{note.content}</div>
+                        <span className="DelIcon"><DeleteIcon /></span>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
