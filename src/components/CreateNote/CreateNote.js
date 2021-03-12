b593fb10dc5cc758bed3565b795395551089fad8
Swarnab Garang <swarnabgarang@gmail.com>
2021-03-13 00:53:27 +0530

    linked client-side to server-side

--- src/components/CreateNote/CreateNote.js
+++ src/components/CreateNote/CreateNote.js
@@ -1,16 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useHistory } from 'react-router';
 import './CreateNote.css'
 
 const CreateNote = () => {
+
+    const [content, setContent] = useState('');
+    const history = useHistory();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const note = { content }
+
+        fetch('http://localhost:8000/notes', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(note)
+        }).then(() => {
+            console.log('New Note Added')
+            history.push('/');
+        })
+    }
+
     return (
         <div className="CreateForm">
             <div className="FormContent">
-                <form action="">
+                <form onSubmit={handleSubmit}>
                     <div className="NoteForm">
                         <h3 className="TextHead">Note</h3>
-                        <textarea className="NoteText" />
+                        <textarea
+                            className="NoteText"
+                            required
+                            value={content}
+                            onChange={(e) => setContent(e.target.value)} />
                     </div>
-                    <button className="CreateNoteBtn">Create Note</button>
+                    <button className="CreateNoteBtn" onClick={handleSubmit} >Create Note</button>
                 </form>
             </div>
         </div>
