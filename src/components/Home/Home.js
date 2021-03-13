5ea238c3258bb48ca364e53daefd1b7fe0d58db6
Swarnab Garang <swarnabgarang@gmail.com>
2021-03-13 19:32:11 +0530

    removed useFetch hook and used axios instead

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,27 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import './Home.css'
+import axios from 'axios';
 
 import DeleteIcon from '@material-ui/icons/Delete';
 
 const Home = () => {
 
-    const notes = [{
-        content: 'Task 1',
-        completed: true,
-        createdAt: 'yesterday'
-    },
-    {
-        content: 'Task 2',
-        completed: true,
-        createdAt: 'daybefore'
-    },
-    {
-        content: 'Task 3',
-        completed: true,
-        createdAt: 'daybefore'
+    const [noteList, setNotes] = useState(null);
+
+    useEffect(async () => {
+        const notes = await (axios.get('http://localhost:8000/notes')).data
+        try {
+            setNotes(notes);
+        } catch (err) {
+            console.log(err.message)
+        }
+    })
+
+    useEffect(async () => {
+
+        const notes = await (axios.get('http://localhost:8000/notes')).data
+
+        try {
+            setNotes(notes);
+        } catch (err) {
+            console.log(err.message);
+        }
+    }, [noteList, setNotes]);
+
+    const handleDelete = (id) => {
+        fetch('http://localhost:8000/notes/' + id, {
+            method: "DELETE"
+        })
+            .then(() => {
+                setNotes(
+                    noteList.map((note) => {
+                        return note._id !== id;
+                    })
+                )
+            })
     }
-    ]
 
     return (
         <div className="Home">
@@ -30,12 +49,16 @@ const Home = () => {
             <Link to='/create'><button className="AddBtn">+</button></Link>
 
             <div className="NoteList">
-                {notes.map((note) => (
+
+
+                {noteList && <div> {noteList.map((note) => (
                     <div className="Note">
                         <div className="NoteContent">{note.content}</div>
-                        <span className="DelIcon"><DeleteIcon /></span>
+                        <span className="DelIcon" onClick={() => {
+                            handleDelete(note._id)
+                        }} ><DeleteIcon /></span>
                     </div>
-                ))}
+                ))} </div>}
             </div>
         </div>
     )
